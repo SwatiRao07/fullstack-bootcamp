f33affe85c14241d8b325186942355a1bca8215d
Swati Rao <swati200102@gmail.com>
2026-02-22 12:14:18 +0000

    Started with functions sub-section

---
+++ typescript/src/ts-functions/drill-01.ts
@@ -0,0 +1,11 @@
+function addition(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(addition(5,5))
+
+function logMessage(message: string): void {
+  console.log(message);
+}
+
+logMessage("hey")
